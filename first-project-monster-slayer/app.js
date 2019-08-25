28ad1ad0851d6c8c348f5906c87ece29771d06cd
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-08-25 15:17:53 -0400

    Finishing the log.

--- first-project-monster-slayer/app.js
+++ first-project-monster-slayer/app.js
@@ -11,6 +11,7 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function() {
       var damage = this.calculateDamage(3, 10);
@@ -25,7 +26,12 @@ new Vue({
       this.monsterAttacks();
     },
     specialAttack: function() {
-      this.monsterHealth -= this.calculateDamage(10, 20);
+      var damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: false,
+        text: "Player hits Monster hard for " + damage
+      });
       if (this.checkWin()) {
         return;
       }
@@ -38,6 +44,10 @@ new Vue({
       } else {
         this.playerHealth = 100;
       }
+      this.turns.unshift({
+        isPlayer: false,
+        text: "Player heals for 10"
+      });
     },
     giveUp: function() {
       this.gameIsRunning = false;
@@ -48,7 +58,7 @@ new Vue({
       this.checkWin();
       this.turns.unshift({
         isPlayer: false,
-        text: "Monster hits player for " + damage
+        text: "Monster hits Player for " + damage
       });
     },
     calculateDamage: function(min, max) {

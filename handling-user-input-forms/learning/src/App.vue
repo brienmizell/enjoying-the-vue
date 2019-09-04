b90a50207f8d55f785a2207130cb29280fec0697
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-09-03 22:54:16 -0400

    Created a custom control and handling form submit.

--- handling-user-input-forms/learning/src/App.vue
+++ handling-user-input-forms/learning/src/App.vue
@@ -62,15 +62,20 @@
           </select>
         </div>
       </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <app-switch v-model="dataSwitch"></app-switch>
+        </div>
+      </div>
       <hr />
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <button class="btn btn-primary">Submit!</button>
+          <button class="btn btn-primary" @click.prevent="submitted">Submit!</button>
         </div>
       </div>
     </form>
     <hr />
-    <div class="row">
+    <div class="row" v-if="isSubmitted">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <div class="panel-heading">
@@ -89,7 +94,7 @@
             </ul>
             <p>Gender: {{gender}}</p>
             <p>Priority: {{selectedPriority}}</p>
-            <p>Switched:</p>
+            <p>Switched: {{dataSwitch}}</p>
           </div>
         </div>
       </div>
@@ -98,6 +103,8 @@
 </template>
 
 <script>
+import Switch from "./Switch.vue";
+
 export default {
   data() {
     return {
@@ -110,8 +117,18 @@ export default {
       sendMail: [],
       gender: "",
       selectedPriority: "High",
-      priorities: ["High", "Medium", "Low"]
+      priorities: ["High", "Medium", "Low"],
+      dataSwitch: true,
+      isSubmitted: false
     };
+  },
+  methods: {
+    submitted() {
+      this.isSubmitted = true;
+    }
+  },
+  components: {
+    appSwitch: Switch
   }
 };
 </script>

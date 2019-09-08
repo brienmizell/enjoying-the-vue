b23f6923297e3f9928371f6579cbd2db661732fc
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-09-08 16:39:54 -0400

    Finished exercise 3.

--- handling-user-input-forms/practice/src/App.vue
+++ handling-user-input-forms/practice/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <form>
+    <form v-if="!submitted">
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <!-- Exercise 1 -->
@@ -33,6 +33,11 @@
               <input type="radio" value="No" v-model="storeData" /> No
             </label>
           </div>
+          <button
+            type="submit"
+            class="btn btn-primary"
+            @click.prevent="submitted = true"
+          >Submit the Form</button>
           <!-- Exercise 2 -->
           <!-- Only display the Form if it has NOT been submitted -->
           <!-- Display the Data Summary ONCE the Form HAS been submitted -->
@@ -44,7 +49,7 @@
       </div>
     </form>
     <hr />
-    <div class="row">
+    <div class="row" v-if="submitted">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <div class="panel-heading">
@@ -70,7 +75,8 @@ export default {
       firstName: "",
       lastName: "",
       password: "",
-      storeData: true
+      storeData: "Yes",
+      submitted: false
     };
   }
 };
